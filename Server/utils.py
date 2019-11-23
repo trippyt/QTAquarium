be5aa2d0d74011364f6f283665831da10194c16d
trippyt <travisgleeson.94@gmail.com>
2019-11-23 01:45:09 +0000

    changed setup for led

--- Server/utils.py
+++ Server/utils.py
@@ -96,9 +96,13 @@ class AquariumController():
         self.led_loop = True
 
     def notification_led_flash(self):
+        self.notification_led_stop()
+        asyncio.sleep(0.01)
         self.led_task = asyncio.run_coroutine_threadsafe(self.led(FLASH), self.event_loop)
 
     def notification_led_pulse(self):
+        self.notification_led_stop()
+        asyncio.sleep(0.01)
         self.led_task = asyncio.run_coroutine_threadsafe(self.led(PULSE), self.event_loop)
 
     def notification_led_stop(self):
