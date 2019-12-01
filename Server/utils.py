6a5772c5c502316caeb031fe6c40e7e914ad9230
trippyt <travisgleeson.94@gmail.com>
2019-12-01 17:01:15 +0000

    added logging

--- Server/utils.py
+++ Server/utils.py
@@ -1,6 +1,13 @@
 import asyncio
+import time
+import logging
 from BaseFunctions import AquariumController
 ac = AquariumController
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 
 def start_calibration(pump_type: str):
@@ -8,25 +15,24 @@ def start_calibration(pump_type: str):
         cal_time = None
         ac.notification_led_pulse()
         ac.button_state()
-        if pump_type == 'co2':
-            print(f"Running {pump_type}")
-            print(f"{pump_type}                      Calibration started.")
-            ac.notification_led_flash()
-            start = time.time()
-            ac.pump_on(pump_type)
-            ac.button_state()
-            print(f"Stopping {pump_type}")
-            print(f"{pump_type}                      Calibration finished.")
-            end = time.time()
-            ac.pump_off()
-            cal_time = round(end - start, 2)
-            co2_per_ml = round(cal_time/10, 2)
-            print(cal_time)
-            ac.notification_led_stop()
+        logging.log(f"Running {pump_type}")
+        logging.log(f"{pump_type}                      Calibration started.")
+        ac.notification_led_flash()
+        start = time.time()
+        ac.pump_on(pump_type)
+        ac.button_state()
+        logging.log(f"Stopping {pump_type}")
+        logging.log(f"{pump_type}                      Calibration finished.")
+        end = time.time()
+        ac.pump_off()
+        cal_time = round(end - start, 2)
+        co2_per_ml = round(cal_time/10, 2)
+        logging.log(cal_time)
+        ac.notification_led_stop()
     except:
         pass
 
 
 async def stop_pump(pump_type: str):
-    print(f"Stopping {pump_type} Pump")
+    logging.log(f"Stopping {pump_type} Pump")
     ac.pump_off(pump_type)
