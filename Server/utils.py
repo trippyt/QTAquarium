a18fb1d22f71a16b1773facf8e97e87a7912d08b
trippyt <travisgleeson.94@gmail.com>
2019-12-15 00:30:52 +0000

    added start_pump to utils.py

--- Server/utils.py
+++ Server/utils.py
@@ -22,18 +22,23 @@ def start_calibration(pump_type: str):
         logging.log(f"{pump_type}                      Calibration started.")
         Ac.notification_led_flash()
         start = time.time()
-        Ac.pump_on(pump_type)
+        start_pump(pump_type)
         Ac.button_state()
         logging.log(f"Stopping {pump_type}")
         logging.log(f"{pump_type}                      Calibration finished.")
         end = time.time()
-        Ac.pump_off()
+        stop_pump(pump_type)
         cal_time = round(end - start, 2)
         co2_per_ml = round(cal_time/10, 2)
         logging.log(cal_time)
         Ac.notification_led_stop()
     except CalibrationCancelled:
         print("!Calibration was Cancelled!")
+
+
+async def start_pump(pump_type: str):
+    logging.log(f"Stopping {pump_type} Pump")
+    Ac.pump_on(pump_type)
 
 
 async def stop_pump(pump_type: str):
