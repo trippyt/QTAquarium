6288eda7422e514de7ee75f0bc46c5a05a465ced
trippyt <travisgleeson.94@gmail.com>
2019-12-15 13:19:47 +0000

    working on calibration function

--- Server/utils.py
+++ Server/utils.py
@@ -20,29 +20,29 @@ def start_calibration(pump_type: str):
         cal_time = None
         controller.notification_led_pulse()
         controller.button_state()
-        logging.log(f"Running {pump_type}")
-        logging.log(f"{pump_type}                      Calibration started.")
+        logging.debug(f"Running {pump_type}")
+        logging.debug(f"{pump_type}                      Calibration started.")
         controller.notification_led_flash()
         start = time.time()
         start_pump(pump_type)
         controller.button_state()
-        logging.log(f"Stopping {pump_type}")
-        logging.log(f"{pump_type}                      Calibration finished.")
+        logging.debug(f"Stopping {pump_type}")
+        logging.debug(f"{pump_type}                      Calibration finished.")
         end = time.time()
         stop_pump(pump_type)
         cal_time = round(end - start, 2)
         co2_per_ml = round(cal_time/10, 2)
-        logging.log(cal_time)
+        logging.debug(cal_time)
         controller.notification_led_stop()
     except CalibrationCancelled:
         print("!Calibration was Cancelled!")
 
 
 async def start_pump(pump_type: str):
-    logging.log(f"Starting {pump_type} Pump")
+    logging.debug(f"Starting {pump_type} Pump")
     Ac.pump_on(pump_type)
 
 
 async def stop_pump(pump_type: str):
-    logging.log(f"Stopping {pump_type} Pump")
+    logging.debug(f"Stopping {pump_type} Pump")
     Ac.pump_off(pump_type)
