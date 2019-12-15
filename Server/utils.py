e70135413ec3f8bf8d2fa0e7b505ff0eefacb5b5
trippyt <travisgleeson.94@gmail.com>
2019-12-15 18:16:01 +0000

    working on calibration function

--- Server/utils.py
+++ Server/utils.py
@@ -20,29 +20,29 @@ async def start_calibration(pump_type: str):
         cal_time = None
         controller.notification_led_pulse()
         controller.button_state()
-        logging.debug(f"Running {pump_type}")
-        logging.debug(f"{pump_type}                      Calibration started.")
+        logging.info(f"Running {pump_type}")
+        logging.info(f"{pump_type}                      Calibration started.")
         controller.notification_led_flash()
         start = time.time()
-        await start_pump(pump_type)
+        start_pump(pump_type)
         controller.button_state()
-        logging.debug(f"Stopping {pump_type}")
-        logging.debug(f"{pump_type}                      Calibration finished.")
+        logging.info(f"Stopping {pump_type}")
+        logging.info(f"{pump_type}                      Calibration finished.")
         end = time.time()
-        await stop_pump(pump_type)
+        stop_pump(pump_type)
         cal_time = round(end - start, 2)
         co2_per_ml = round(cal_time/10, 2)
-        logging.debug(cal_time)
+        logging.info(cal_time)
         controller.notification_led_stop()
     except CalibrationCancelled:
         print("!Calibration was Cancelled!")
 
 
-async def start_pump(pump_type: str):
-    logging.debug(f"Starting {pump_type} Pump")
+def start_pump(pump_type: str):
+    logging.info(f"Starting {pump_type} Pump")
     Ac.pump_on(pump_type)
 
 
-async def stop_pump(pump_type: str):
-    logging.debug(f"Stopping {pump_type} Pump")
+def stop_pump(pump_type: str):
+    logging.info(f"Stopping {pump_type} Pump")
     Ac.pump_off(pump_type)
