0934a5e23e50426d6b8a9dea695b5c6634edd1e9
trippyt <travisgleeson.94@gmail.com>
2019-12-16 17:19:45 +0000

    working on displaying temperature from server to client

--- Server/utils.py
+++ Server/utils.py
@@ -38,5 +38,5 @@ def stop_pump(pump_type: str):
 
 
 def tank_temperature():
-    temp_c, temp_f = controller.read_temperature(temp_sensor_type)
+    temp_c, temp_f = controller.read_temperature("temp_tank")
     return round(temp_c, 2)
