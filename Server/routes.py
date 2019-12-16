87049cf8998f50b622c7775acbd752daa103d9d2
trippyt <travisgleeson.94@gmail.com>
2019-12-16 17:10:11 +0000

    working on displaying temperature from server to client

--- Server/routes.py
+++ Server/routes.py
@@ -9,7 +9,7 @@ app = Quart(__name__)
 @app.websocket('/temp')
 async def temp():
     while True:
-        temp = await utils.tank_temperature('W1ThermSensor')
+        temp = await utils.tank_temperature(temp_sensor_type, W1ThermSensor)
         print(temp)
         await asyncio.sleep(2)
         await websocket.send(str(temp))
