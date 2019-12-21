90968754fa3ef4770948c5626fbc05fde24cba9b
trippyt <travisgleeson.94@gmail.com>
2019-12-21 13:50:33 +0000

    working ratios

--- Server/BaseFunctions.py
+++ Server/BaseFunctions.py
@@ -146,7 +146,7 @@ class AquariumController:
          'Fertilizer Concentrate: {} mL, Fertilizer to Water: {} Litres,\n'
          'WaterConditioner Concentrate: {} mL, WaterConditioner to Water: {} Litres'.format(
             *ratio_results))
-        self.ratioequals()
+        self.ratioequals(ratio_results)
 
 
     def calibrate_pump(self, pump_type):
