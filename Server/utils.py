13ece0a4f1a9715d01006c43d0edf6175a89988d
trippyt <travisgleeson.94@gmail.com>
2020-03-06 17:08:42 +0000

    Possible overhaul

--- Server/utils.py
+++ Server/utils.py
@@ -1,5 +1,7 @@
 import asyncio
 import time
+inport os
+import json
 import logging
 from AquariumHardware2 import Hardware
 logging.basicConfig(
@@ -29,58 +31,58 @@ class AquariumController:
             "Water Conditioner Ratio": {},
         }
 
-        async def start_calibration(self, pump_type: str):
-            try:
-                await self.hw_controller.notification_led_pulse()
-                self.hw_controller.button_state()
-                await self.hw_controller.notification_led_flash()
-                self.calibrate_pump(pump_type)
-                await self.hw_controller.notification_led_stop()
-            except CalibrationCancelled:
-                print("!Calibration was Cancelled!")
+    async def start_calibration(self, pump_type: str):
+        try:
+            await self.hw_controller.notification_led_pulse()
+            self.hw_controller.button_state()
+            await self.hw_controller.notification_led_flash()
+            self.calibrate_pump(pump_type)
+            await self.hw_controller.notification_led_stop()
+        except CalibrationCancelled:
+            print("!Calibration was Cancelled!")
 
-        def calibrate_pump(self, pump_type):
-            logging.info(f"Running {pump_type} Pump")
-            logging.info(f"{pump_type}                      Calibration started.")
-            self.calibration_status(pump_type, self.cal_status[2])
-            start = time.time()
-            self.pump_on(pump_type)
-            self.button_state()
-            logging.info(f"Stopping {pump_type}")
-            logging.info(f"{pump_type}                      Calibration finished.")
-            self.calibration_status(pump_type, self.cal_status[0])
-            end = time.time()
-            self.pump_off(pump_type)
-            cal_time = round(end - start, 2)
-            per_ml = round(cal_time / 10, 2)
-            print(type(cal_time))
-            logging.info(f"{pump_type} Runtime: {cal_time}")
-            self.calibration_data[f"{pump_type} Calibration Data"].update(
-                {
-                    "Time per 10mL": cal_time,
-                    "Time per 1mL": per_ml
-                }
-            )
-            self.save()
+    def calibrate_pump(self, pump_type):
+        logging.info(f"Running {pump_type} Pump")
+        logging.info(f"{pump_type}                      Calibration started.")
+        self.calibration_status(pump_type, self.cal_status[2])
+        start = time.time()
+        self.pump_on(pump_type)
+        self.button_state()
+        logging.info(f"Stopping {pump_type}")
+        logging.info(f"{pump_type}                      Calibration finished.")
+        self.calibration_status(pump_type, self.cal_status[0])
+        end = time.time()
+        self.pump_off(pump_type)
+        cal_time = round(end - start, 2)
+        per_ml = round(cal_time / 10, 2)
+        print(type(cal_time))
+        logging.info(f"{pump_type} Runtime: {cal_time}")
+        self.calibration_data[f"{pump_type} Calibration Data"].update(
+            {
+                "Time per 10mL": cal_time,
+                "Time per 1mL": per_ml
+            }
+        )
+        self.save()
 
-        def stop_calibration(self, pump_type: str):
-            self.hw_controller.stop_calibration()
+    def stop_calibration(self, pump_type: str):
+        self.hw_controller.stop_calibration()
 
-        def cal_status(self,pump_type: str):
-            self.hw_controller.calibration_status()
+    def cal_status(self,pump_type: str):
+        self.hw_controller.calibration_status()
 
-        def tank_temperature(self):
-            temp_c, temp_f = self.hw_controller.read_temperature("temp_tank")
-            return round(temp_c, 2)
+    def tank_temperature(self):
+        temp_c, temp_f = self.hw_controller.read_temperature("temp_tank")
+        return round(temp_c, 2)
 
-        def alert_data(self,ht, lt):
-            self.hw_controller.alert_data(ht, lt)
+    def alert_data(self,ht, lt):
+        self.hw_controller.alert_data(ht, lt)
 
-        def email_alert(self):
-            pass
+    def email_alert(self):
+        pass
 
-        def newRatios(self,ratio_results: str):
-            self.hw_controller.ratios(ratio_results)
+    def newRatios(self,ratio_results: str):
+        self.hw_controller.ratios(ratio_results)
 
     def load(self):
         if os.path.isfile('data.txt'):
