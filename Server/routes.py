1c96628cad1739a32d77e816a9f17dbaf16403c5
trippyt <travisgleeson.94@gmail.com>
2020-03-06 16:50:19 +0000

    Possible overhaul

--- Server/routes.py
+++ Server/routes.py
@@ -74,7 +74,7 @@ async def calibration_status():
 @app.websocket('/temp')
 async def temp():
     while True:
-        temp = controller.self.tank_temperature()
+        temp = controller.tank_temperature()
         print(temp)
         await asyncio.sleep(2)
         await websocket.send(str(temp))
