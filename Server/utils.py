88578430a98f793c28b4d14fbbfa2b5c5e3cd2f6
trippyt <travisgleeson.94@gmail.com>
2020-03-12 22:20:26 +0000

    Working on email Alerts

--- Server/utils.py
+++ Server/utils.py
@@ -196,7 +196,7 @@ class AquariumController:
                     print("Loading Saved Data")
                     self.ratio_data = data["Ratio Data"]
                     self.calibration_data = data["Calibration Data"]
-                    self.setting_data = data["Temperature Alerts"]
+                    #self.setting_data = data["Temperature Alerts"]
                     # conversion_values
                     # schedule_data
                     # light_hour_data
