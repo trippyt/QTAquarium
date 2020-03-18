311c4ba85613b8645c4a4c89a5c26c3109e4b142
trippyt <travisgleeson.94@gmail.com>
2020-03-18 23:05:45 +0000

    Working on email Alerts

--- Server/email_alert.py
+++ Server/email_alert.py
@@ -47,7 +47,8 @@ class EmailAlerts:
             self.alert_limit = int(self.config_data["network_config"]["alert_limit"])
             self.cur_date = datetime.datetime.utcnow().strftime('%m-%d-%Y')
             self.cur_time = datetime.datetime.utcnow().strftime('%H:%M:%S')
-            self.prev_date = self.alert_counter["{} Last Date Called".format(str(alert_type))]
+            print(f"alert_type TYPE:{(type(alert_type))}")
+            self.prev_date = self.alert_counter["{} Last Date Called".format(alert_type)]
             self.prev_time = self.alert_counter[f"{alert_type} Last Time Called"]
             self.alerts_sent = self.alert_counter[f"{alert_type}"]
         except Exception as e:
