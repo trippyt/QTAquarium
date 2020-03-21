9a48a2ffe638d80ddd32bd093cf7e5da1e57f159
trippyt <travisgleeson.94@gmail.com>
2020-03-21 03:24:00 +0000

    Working on email Alerts

--- Server/email_alert.py
+++ Server/email_alert.py
@@ -184,10 +184,12 @@ class EmailAlerts:
         logger.info("=" * 125)
         logger.info(f"Alert Type: {alert_type}")
         logger.info(f"config before counter: {self.alert_counter}")
+        self.refresh_data()
         # cur_datetime = datetime.datetime.utcnow().strftime('%m-%d-%Y - %H:%M:%S')
         try:
             if alert_type in self.alert_counter.keys():
                 self.refresh_time_var(alert_type)
+
                 logger.info(f"Updating {alert_type} Counter")
                 self.alert_counter[f"{alert_type}"] += 1
                 self.alert_counter[f"{alert_type} Last Date Called"] = self.cur_date
