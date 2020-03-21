bdc64473b9d12514407b8d015b69cf51fe8577c9
trippyt <travisgleeson.94@gmail.com>
2020-03-21 02:14:01 +0000

    Working on email Alerts

--- Server/email_alert.py
+++ Server/email_alert.py
@@ -159,7 +159,7 @@ class EmailAlerts:
         return self.alert_counter
 
     def create_counter(self, alert_type):
-        self.alert_counter[f"{alert_type}"].update(
+        self.alert_counter[f"{alert_type}"].append(
             {
                 f"{alert_type}": 0,
                 f"{alert_type} Last Date Called": "Never",
