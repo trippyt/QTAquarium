68fb69a26d338e58492f3dccc335b56fe84a78bc
trippyt <travisgleeson.94@gmail.com>
2020-03-24 17:39:10 +0000

    Working on email Alerts

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
 from routes import app
 from quart import Quart, request
+from loguru import logger
 
 
 def is_production():
@@ -9,9 +10,12 @@ def is_production():
     Compare root location against developer server value 127.0.0.1:5000.
     :return: (bool) True if code is running on the production server, and False otherwise.
     """
-    root_url = request.url_root
-    developer_url = 'http://0.0.0.0:5000/'
-    return root_url != developer_url
+    try:
+        root_url = request.url_root
+        developer_url = 'http://0.0.0.0:5000/'
+        return root_url != developer_url
+    except RuntimeError:
+        logger.exception(f"ooops2")
 
 
 print(is_production())
