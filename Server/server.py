7e25c01830a2f7c8d63e37449741cb0ef6383c83
trippyt <travisgleeson.94@gmail.com>
2020-03-24 18:04:29 +0000

    Working on email Alerts

--- Server/server.py
+++ Server/server.py
@@ -2,29 +2,23 @@ from routes import app
 from loguru import logger
 import requests.exceptions
 
-def is_production():
-    """ Determines if app is running on the production server or not.
-    Get Current URI.
-    Extract root location.
-    Compare root location against developer server value 127.0.0.1:5000.
-    :return: (bool) True if code is running on the production server, and False otherwise.
-    """
+
+def server_check():
     try:
+        r = requests.get('http://192.168.1.33:5000')
+        r.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xxx
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        logger.exception("Down")
+    except requests.exceptions.HTTPError:
+        logger.exception("4xx, 5xx")
+    else:
+        logger.debug("All good!")  # Proceed to do stuff with `r`
+        return r.raise_for_status()
 
 
-        try:
-            r = requests.get('http://example.com')
-            r.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xxx
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            logger.debug("Down")
-        except requests.exceptions.HTTPError:
-            logger.debug("4xx, 5xx")
-        else:
-            logger.debug("All good!")  # Proceed to do stuff with `r`
-    except RuntimeError:
-        logger.exception(f"ooops2")
+def start_server():
+    if __name__ == '__main__':
+        app.run("0.0.0.0")
 
 
-print(is_production())
-if __name__ == '__main__':
-    app.run("0.0.0.0")
+print(server_check())
