71a3b452b0aa7fd3a2f71ee00b96b3e9a52a2cfd
trippyt <travisgleeson.94@gmail.com>
2020-05-03 16:42:05 +0100

    Working on Graph Data

--- Server/routes.py
+++ Server/routes.py
@@ -150,7 +150,7 @@ def hello_world():
 async def csv():
     while True:
         csv_data = controller.get_csv()
-        print(csv_data)
+        logger.debug(len(csv_data))
         if csv_data is not None:
             await websocket.send(csv_data)
 
