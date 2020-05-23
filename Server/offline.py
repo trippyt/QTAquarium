12b5930c47aadd0e72b935c2a83317bc45293411
trippyt <travisgleeson.94@gmail.com>
2020-05-23 20:08:39 +0100

    Working on Graph Data

--- Server/offline.py
+++ Server/offline.py
@@ -93,7 +93,8 @@ class OfflineFunctions:
     def sql_insert(self, con, entities):
         cursorObj = con.cursor()
         cursorObj.execute(
-            '''INSERT INTO tank_temperature VALUES(%s, %s, %s, %s)''', entities)
+            '''INSERT INTO tank_temperature (date, time, temperature_c, temperature_f) VALUES(%s, %s, %s, %s)''',
+            entities)
         con.commit()
 
     def monitor_temperature(self):
