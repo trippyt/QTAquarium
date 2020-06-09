d2458ff0d61b680f13348744789ab9ae196fcfe5
trippyt <travisgleeson.94@gmail.com>
2020-06-09 15:47:38 +0100

    added DHT22 Sensor

--- Server/AquariumHardware2.py
+++ Server/AquariumHardware2.py
@@ -63,7 +63,8 @@ class Hardware:
 
     def room_temperature(self):
         try:
-            result = sensor_dht.sample(samples=1)
+            # result = sensor_dht.sample(samples=1)
+            result = sensor_dht.read(retries=0)
             return result
         except TimeoutError as error:
             logger.warning(error.args[0])
