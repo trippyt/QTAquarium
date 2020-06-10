88973ee3a33cb34cb75288fd4f5d94887d6e3192
trippyt <travisgleeson.94@gmail.com>
2020-06-10 11:31:23 +0100

    added DHT22 Sensor

--- Server/dht_filtered.py
+++ Server/dht_filtered.py
@@ -56,17 +56,17 @@ def readingValues():
                     [temp_c, temp_f, humidity] = dht
                 logger.exception(dht)
 
+                if math.isnan(temp_c) is False and math.isnan(temp_f) is False and math.isnan(humidity) is False:
+                    values.append({"temp_c": temp_c, "temp_f": temp_f, "hum": humidity})
+                    counter += 1
+                # else:
+                # print("we've got NaN")
+
             except IOError:
                 print("we've got IO error")
 
             except Exception as error:
                 logger.exception(error.args[0])
-
-            if math.isnan(temp_c) is False and math.isnan(temp_f) is False and math.isnan(humidity) is False:
-                values.append({"temp_c": temp_c, "temp_f": temp_f, "hum": humidity})
-                counter += 1
-            # else:
-            # print("we've got NaN")
 
             sleep(1)
 
